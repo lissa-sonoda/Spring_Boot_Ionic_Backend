a9861bc12f4abfdf9f03f078e9b5b295f183fc7b
Lissa Sonoda <helen.lissa@gmail.com>
2022-02-23 18:06:46 -0300

    Category Class - Tested

---
+++ src/main/java/com/educandoweb/cursomc/domain/Category.java
@@ -0,0 +1,50 @@
+package com.educandoweb.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Category(){
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id != null ? id.equals(category.id) : category.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
--- src/main/java/com/educandoweb/cursomc/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/cursomc/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.educandoweb.cursomc.resources;
 
+import com.educandoweb.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list(){
-        return "REST está funcionando!";
+    public List<Category> listing(){
+
+        Category cat1 = new Category(1, "Informatics");
+        Category cat2 = new Category(2, "Office");
+
+        List<Category> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 }
