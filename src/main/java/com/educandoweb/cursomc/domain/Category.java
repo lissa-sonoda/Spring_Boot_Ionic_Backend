0955f92efb2e34fef618daf49248e4ef3258459d
Lissa Sonoda <helen.lissa@gmail.com>
2022-02-24 08:41:42 -0300

    H2 database

--- src/main/java/com/educandoweb/cursomc/domain/Category.java
+++ src/main/java/com/educandoweb/cursomc/domain/Category.java
@@ -1,11 +1,18 @@
 package com.educandoweb.cursomc.domain;
 
-import java.io.Serializable;
-import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import java.io.Serializable;
+
+@Entity
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
