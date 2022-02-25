e5a1b1c3088be6942a7bcfb300b9f1befb65d12b
Lissa Sonoda <helen.lissa@gmail.com>
2022-02-25 13:56:17 -0300

    Insert New Category - Method Post

--- src/main/java/com/educandoweb/cursomc/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/cursomc/resources/CategoryResource.java
@@ -4,10 +4,10 @@ import com.educandoweb.cursomc.domain.Category;
 import com.educandoweb.cursomc.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/categories")
@@ -20,5 +20,13 @@ public class CategoryResource {
     public ResponseEntity<?> find(@PathVariable Integer id){
         Category obj = service.search(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Category obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
